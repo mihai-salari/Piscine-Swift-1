7eb5b0feb32d0196f9fd09635f4d070950e59ea9
luc Casimir <Owee@MacBook-Pro-de-luc.local>
2016-10-12 07:36:10 +0200

    D06

--- D06/D06/Item.swift
+++ D06/D06/Item.swift
@@ -15,52 +15,79 @@ enum Form : UInt {
 
 class Item: UIView {
     
-//    let elasticity : UIDynamicItemBehavior = {
-//    }
-    }
+    let elasticity : UIDynamicItemBehavior = {
+        let did = UIDynamicItemBehavior()
+        did.allowsRotation = true
+        did.elasticity = 0.75
+        return did
+    }()
+    
+    let sizeItem = 100
     let gravity = UIGravityBehavior()
     let collider : UICollisionBehavior = {
         let collider = UICollisionBehavior()
         collider.translatesReferenceBoundsIntoBoundary = true
         return collider
     }()
+
     lazy var animator : UIDynamicAnimator = UIDynamicAnimator(referenceView: self)
     var animation : Bool = false{
         didSet{
             if animation{
                 animator.addBehavior(gravity)
                 animator.addBehavior(collider)
+                animator.addBehavior(elasticity)
             }
             else{
                 animator.removeBehavior(gravity)
                 animator.removeBehavior(collider)
+                animator.removeBehavior(elasticity)
             }
         }
     }
 
     func getRandomColor() -> UIColor
     {
-        switch arc4random() % 3 {
+        switch arc4random() % 6 {
         case 0:
             return UIColor.redColor()
         case 1:
             return UIColor.blueColor()
         case 2:
             return UIColor.greenColor()
+        case 3:
+            return UIColor.yellowColor()
+        case 4:
+            return UIColor.cyanColor()
+        case 5:
+            return UIColor.purpleColor()
         default:
             return UIColor.blackColor()
+        }
+    }
+
+    func getRandomForm() -> CGFloat{
+        switch arc4random() % 2 {
+        case 0:
+            return 0
+        default:
+            return CGFloat(sizeItem / 2)
         }
     }
     
     func add(point:CGPoint)
     {
-        let frame = CGRect(origin:CGPoint(x: point.x - 50, y: point.y - 50), size: CGSize(width: 100, height: 100))
+        let frame = CGRect(origin:CGPoint(x: point.x - CGFloat(sizeItem / 2), y: point.y - CGFloat(sizeItem/2)), size: CGSize(width: sizeItem, height: sizeItem))
         let item = UIView(frame: frame)
         item.backgroundColor = getRandomColor()
-        item.layer.cornerRadius = 50
+        item.layer.cornerRadius = getRandomForm()
         item.clipsToBounds = true
         addSubview(item)
         gravity.addItem(item)
         collider.addItem(item)
+        elasticity.addItem(item)
+    }
+    override var collisionBoundsType: UIDynamicItemCollisionBoundsType{
+        return .Ellipse
     }
 }
